7d13a71d4b2fdd6db7e005d04dd0fa66c3b8f2f1
Md. Rafiuzzaman Bhuiyan Afridi <35966401+whoafridi@users.noreply.github.com>
2018-10-08 21:45:09 +0600

    initial comit

---
+++ Algorithms/BinarySearch.py
@@ -0,0 +1,18 @@
+## Fun fact in Py
+
+def BinarySearch(L, x):
+    left, right = 0, len(L)-1
+    while left <= right:
+        mid = (left + right)//2 
+        if L[mid] == x:
+            return mid       
+        if L[mid] < x:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return 'Not found  
+    
+if __name__ == "__main__":
+    List1 = [1, 2, 3, 5, 6, 7, 8, 9]
+    key = 9
+    print(BinarySearch(List1,key))  ## key found in index 7 
