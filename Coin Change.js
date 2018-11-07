4285b37a450808d93be5b9145a5654254c0d9815
Md. Rafiuzzaman Bhuiyan Afridi <35966401+whoafridi@users.noreply.github.com>
2018-11-07 22:06:15 +0600

    updated

--- Coin Change.js
+++ Coin Change.js
@@ -5,7 +5,8 @@ for (i=0;i < BDT.length;i++){
   num =  Math.floor(amount / BDT[i]);
       amount -= num*BDT[i];
       noofcoins = noofcoins + num;
-  }
+      console.log(BDT[i],'TK coin need =',num);
+  }  
 }
 return noofcoins
 }
@@ -13,4 +14,6 @@ return noofcoins
 BDT = [1000,500,100,50,20,5,2,1];
 amount = 779;
 noofcoins = 0;
-console.log('No. of coins needed to be paid',min_coins(BDT,noofcoins,amount));
+console.log('No. of coins needed to be paid = ',min_coins(BDT,noofcoins,amount));
+
+
