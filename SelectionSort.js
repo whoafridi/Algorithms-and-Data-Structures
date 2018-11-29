3d62ea88654dd5aa29c1a8fd97d8cd8194c5b6bc
Md. Rafiuzzaman Bhuiyan Afridi <35966401+whoafridi@users.noreply.github.com>
2018-11-29 23:03:19 +0600

    added

---
+++ SelectionSort.js
@@ -0,0 +1,17 @@
+// Author : Afridi
+
+function Selection_sort(L){
+	var n = L.length;
+	for(i=0 ; i<n-1 ; i++){
+  index=i
+		for(j=i ; j<n ; j++){
+			if (L[j] < L[index]){
+			[L[j],L[index]]=[L[index],L[j]]
+			}
+		}
+	}
+}
+
+var L = [0,6,2,1,4]
+Selection_sort(L)
+console.log('The sorted array',L)
