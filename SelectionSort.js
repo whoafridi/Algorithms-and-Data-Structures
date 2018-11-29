e6800c143c2f1515ff0c664d4be91388b1ebbe50
Md. Rafiuzzaman Bhuiyan Afridi <35966401+whoafridi@users.noreply.github.com>
2018-11-29 23:03:45 +0600

    Update SelectionSort.js

--- SelectionSort.js
+++ SelectionSort.js
@@ -3,7 +3,7 @@
 function Selection_sort(L){
 	var n = L.length;
 	for(i=0 ; i<n-1 ; i++){
-  index=i
+  	    index=i
 		for(j=i ; j<n ; j++){
 			if (L[j] < L[index]){
 			[L[j],L[index]]=[L[index],L[j]]
