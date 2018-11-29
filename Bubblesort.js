38972045e915c1eda34eb83b64d138030b15bce8
Md. Rafiuzzaman Bhuiyan Afridi <35966401+whoafridi@users.noreply.github.com>
2018-11-29 22:19:06 +0600

    added

--- Bubblesort.js
+++ Bubblesort.js
@@ -2,16 +2,16 @@
 // Date : 29-11-2018
 
 function bubble_sort(L){
-	var n = L.length;
-	for(i=0 ; i<n ; i++){
-		for(j=0 ; j<n-i-1 ; j++){
-			if (L[j] > L[j+1]){
-				temp = L[j] ;
-				L[j] = L[j+1] ;
-				L[j+1] = temp ;
-			}
-		}
-	}
+   var n = L.length;
+   for(i=0 ; i<n ; i++){
+       for(j=0 ; j<n-i-1 ; j++){
+	 if (L[j] > L[j+1]){
+	     temp = L[j] ;
+	     L[j] = L[j+1] ;
+	     L[j+1] = temp ;  // use [L[j],L[j+1]] = [L[j+1],L[j]] this for swap in 1 line 
+	 }
+      }
+   }
 }
 
 var L = [0,6,2,1,4]
