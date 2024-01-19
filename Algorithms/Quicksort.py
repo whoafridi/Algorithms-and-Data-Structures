4f45502fcf4acf77eb851b9fe5bcf6799c5f8a09
whoafridi <afridirk10@gmail.com>
2024-01-20 00:39:53 +0600

    quicksort updated

--- Algorithms/Quicksort.py
+++ Algorithms/Quicksort.py
@@ -1,30 +1,15 @@
-## Simple program 
+## A different approach 
 
-def partition(L, low, high):
-  pivot = L[high]
-
-  i = low - 1
-  for j in range(low,high):
-    if L[j] < pivot:
-      i += 1
-      L[i] , L[j] = L[j], L[i]
-
-  L[i+1] , L[high] = L[high] , L[i+1] 
-  return i+1
-
-def Quicksort(L, low,high):
-  if low >= high:
+def Quicksort(L):
+  if len(L) < 2:
     return L
-  p = partition(L,low,high)
-  Quicksort(L,low,p-1)
-  Quicksort(L,p+1,high)
+  else:
+    pivot = L[0]
+    less = [i for i in L[1:] if i <= pivot]
+    greater = [i for i in L[1:] if i > pivot]
+
+    return Quicksort(less) + [pivot] + Quicksort(greater)
 
 if __name__ == "__main__":
-  arr = [10, 7, 8, 9, 1, 5] 
-  n = len(arr) 
-  Quicksort(arr,0,n-1) 
-  for i in range(n): 
-    print ("%d" %arr[i],end=' ') ###  1, 5, 7, 8, 9, 10
-    
-  ### ### ### ### ###
-  
+  l = [0,8,4,9,2,5]
+  print(Quicksort(l))
